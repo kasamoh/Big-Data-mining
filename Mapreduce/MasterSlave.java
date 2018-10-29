d1152287447e566ecd2fddfe0bf1829707b5e79c
DHAOUI MOHAMED <32954393+kasamoh@users.noreply.github.com>
2018-10-29 11:45:44 +0100

    Add files via upload

--- Mapreduce/MasterSlave.java
+++ Mapreduce/MasterSlave.java
@@ -9,6 +9,9 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -42,10 +45,12 @@ public class MasterSlave {
         
     k=0 ;
     Map <String, String> listmachinetask= new HashMap<String, String>();
-   
+    Map <String, String> listkeys= new HashMap<String, String>();
+    Map<String, Collection<String>> mapkeysmachine = new HashMap<String, Collection<String>>();
+    boolean phasemap=true ;
+    
     for (String linetxe:splitStrl){
         	
-    	
     	System.out.println(" ###### lancement du job sur la machine" +linetxe+"#####"); 
     	ProcessBuilder pbtest =new ProcessBuilder("ssh",linetxe ,"hostname") ;
    	    Process ptest=pbtest.start();
@@ -55,7 +60,8 @@ public class MasterSlave {
          // si la machine répond , on lance le slave
          
          if (stdErrortest .readLine()==null){
-            System.out.println(stdErrortest .readLine()==null); // si c'est true ; la machine réponds
+        	 
+         System.out.println("La machine répond : "+String.valueOf(stdErrortest .readLine()==null )); // si c'est true ; la machine réponds
 	 
          /// creation de repository
          ProcessBuilder pbmkdir =new ProcessBuilder("ssh","mdhaoui@"+linetxe,"mkdir","-p","/tmp/mdhaoui/splits") ;
@@ -69,23 +75,10 @@ public class MasterSlave {
          for (int i=0 ; i< 3 ;i++){
              ProcessBuilder pbcopy =new ProcessBuilder("scp","-p","/tmp/mdhaoui/splits/S"+String.valueOf(i)+".txt", "mdhaoui@"+linetxe+":/tmp/mdhaoui/splits") ;
              Process pcopy=pbcopy.start();
-             pcopy.waitFor();
+             //pcopy.waitFor();
          }
+       
          
-
-         
-         //BufferedReader stdErrorcreate = new BufferedReader(new InputStreamReader(pcopy.getErrorStream()));     
-         
-   
-         // si la machine répond , on lance le slave
-         // String retour2 ;
-         // while (( retour2 = stdErrorcreate .readLine()) != null) { //While there's something in the buffer
-             //read&print - replace with a buffered read (into an array) if the output doesn't contain CR/LF
-          	//	 System.out.println("# error #"); 
-          		// System.out.println(retour2); 
-           //}
-            
-         Thread.sleep(2000);
          
          /// creation du dossier map 
          ProcessBuilder pbmkdirmap =new ProcessBuilder("ssh","mdhaoui@"+linetxe,"mkdir","-p","/tmp/mdhaoui/maps") ;
@@ -93,7 +86,7 @@ public class MasterSlave {
          pmkdirmap.waitFor();
          
          
-         System.out.println(" fin copiage"); 
+         System.out.println(" Fin du copiage des splits"); 
          
          /// lancer le slave avec un fichier 
          ProcessBuilder pb =new ProcessBuilder("ssh","mdhaoui@"+linetxe,"java","-jar","/tmp/mdhaoui/SLAVE.jar","map","S"+String.valueOf(k)+".txt") ;
@@ -106,44 +99,40 @@ public class MasterSlave {
          if (b==false){
         	 System.out.println(p.isAlive()); 
         	 p.destroyForcibly();
-		 
+        	 
+        	 phasemap=false ;  // le processus n'est pas fini
          }else{
 		 
              /// on met à jour le dict des machines_task
-        	 listmachinetask.put("UM"+String.valueOf(k)+".txt",linetxe) ;
-
-	 //p.destroy();
-	// p.destroyForcibly();
-	// p.waitFor();
-    // System.out.println(p.isAlive()); 
+        	 listmachinetask.put("UM"+String.valueOf(k),linetxe) ;
 
 
 	
-	
+	         /////////////////////// affichage des keys /////////
              System.out.println("  ######  Standard output #######"); 
-             
-             p.waitFor();
-     
+                 
              
              BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-             Thread.sleep(2000); //Sleep for 2 seconds
 
-             
-	
-	
-	
-        	 //BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        	 // BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        	 // Thread.sleep(2000); //Sleep for 2 seconds
         	 String retour = null;
         	 while ( (retour = stdInput.readLine()) != null) { //While there's something in the buffer
-        		 System.out.println(retour); 
-        	 }
+        	    System.out.println("UM"+String.valueOf(k)+"  "+ retour); 
+
+   			    Collection<String> values = mapkeysmachine.get(retour);
+   			    if (values==null) {
+   			      values = new ArrayList<String>();
+   			      mapkeysmachine.put(retour, values);
+   			    }
+   			  values.add("UM"+String.valueOf(k));
+   			  mapkeysmachine.put(retour,values);
+        		//listkeys.put("UM"+String.valueOf(k), retour) ;
+        	 }	
+        	// listkeys.forEach((kk, v) -> System.out.println(String.format("%s : %s", kk, v)));
+
+
+
       
-    
-      
-      
-      
+            ///////////////////////////// erreur /////////////
 	  
         	 BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
         	 //Thread.sleep(2000); //Sleep for 2 seconds
@@ -160,9 +149,18 @@ public class MasterSlave {
 	}
     
     // print listmachines avec les fichiers de map 
+    
+    System.out.println("############### resultat final ###############");
+    if (phasemap) {
+    	 System.out.println("La partie map a été finie avec succès");
+    }else {
+   	 	 System.out.println("La partie map n'est pas finie avec succès , ci-dessous les résultats des machines qui fonctionnent :");
+	
+    }
     listmachinetask.forEach((kk, v) -> System.out.println(String.format("%s : %s", kk, v)));
 
-    
+	System.out.println(Arrays.asList(mapkeysmachine)); // method 1
+
       
     }
 
